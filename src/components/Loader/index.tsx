09217f98505bed0b5b69ea1afc4c0eb5be4baccd
thomaslnx <marcos.msilva10@gmail.com>
2021-07-15 18:38:07 -0300

    feat: create loader component and move his logic.

---
+++ src/components/Loader/index.tsx
@@ -0,0 +1,74 @@
+import { useEffect, useRef, useState } from 'react';
+import anime from 'animejs';
+import _JSXStyle from 'styled-jsx/style';
+
+import { Preloader, Loading } from './styles';
+
+export default function Loader(): JSX.Element {
+  const preloadRef = useRef(null);
+
+  const [visibility, setVisibility] = useState<string>('visible');
+  const [display, setDisplay] = useState<string | ''>('');
+
+  useEffect(() => {
+    const timeLine = anime
+      .timeline({
+        easing: 'easeInOutCubic',
+        duration: 800,
+        autoplay: false,
+      })
+      .add({
+        targets: '#loader',
+        opacity: 0,
+        duration: 5000,
+        begin: (anim) => {
+          window.scrollTo(0, 0);
+        },
+      })
+      .add({
+        targets: '#preloader',
+        opacity: 0,
+        complete: (anim) => {
+          if (preloadRef.current) {
+            setVisibility('hidden');
+            setDisplay('none');
+          }
+        },
+      });
+
+    timeLine.play();
+
+    (function ssPreloader() {
+      if (!preloadRef.current) return;
+
+      window.addEventListener('load', () => {
+        document.querySelector('html')?.classList.remove('ss-preload');
+        document.querySelector('html')?.classList.add('ss-loaded');
+      });
+
+      timeLine.play();
+    })();
+  }, []);
+
+  return (
+    <>
+      {/* Had to use styled-jsx to fix lac of visibility typescript types */}
+      {/* source: https://github.com/vercel/styled-jsx */}
+      {/* source: https://fettblog.eu/typescript-react/styles/ */}
+      <_JSXStyle>
+        {`
+          div#preload {
+            visibility: ${visibility};
+          }
+        `}
+      </_JSXStyle>
+      <Preloader
+        id="preloader"
+        ref={preloadRef}
+        style={{ display: `${display}` }}
+      >
+        <Loading id="loader" />
+      </Preloader>
+    </>
+  );
+}
---
+++ src/components/Loader/styles.ts
@@ -0,0 +1,52 @@
+import styled from 'styled-components';
+
+export const Preloader = styled.div`
+  position: fixed;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
+  align-items: center;
+  background: var(--color-body);
+  z-index: 500;
+  height: 100vh;
+  width: 100%;
+  opacity: 1;
+
+  #preloader {
+    display: none;
+  }
+`;
+
+export const Loading = styled.div`
+  width: var(--vspace-2);
+  height: var(--vspace-2);
+  padding: 0;
+  background-color: var(--color-white);
+  border-radius: 100%;
+  -webkit-animation: sk-scaleout 1s infinite ease-in-out;
+  animation: sk-scaleout 1s infinite ease-in-out;
+
+  @-webkit-keyframes sk-scaleout {
+    0% {
+      -webkit-transform: scale(0);
+      transform: scale(0);
+    }
+
+    100% {
+      -webkit-transform: scale(1);
+      transform: scale(1);
+      opacity: 0;
+    }
+  }
+
+  @keyframes sk-scaleout {
+    0% {
+      transform: scale(0);
+    }
+
+    100% {
+      transform: scale(1);
+      opacity: 0;
+    }
+  }
+`;
